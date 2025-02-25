29ed6d9211394f02abd48f8e3d7b8741a67c2870
Marvin Emechebe <emechebeonyebuchi@gmail.com>
2025-02-25 20:26:30 +0200

    updated const

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,5 +1,5 @@
 export const ONE_MINUTE = 60 * 1000;
 export const USERS_PER_INTERVAL = 500;
-export const ARTICLES_REQUESTS_PER_MINUTE = 150;
-export const METADATA_REQUESTS_PER_MINUTE = 100;
+export const ARTICLES_REQUESTS_PER_MINUTE = 30;
+export const METADATA_REQUESTS_PER_MINUTE = 30;
 export const FIVE_MINUTES = 5 * 60 * 1000;
